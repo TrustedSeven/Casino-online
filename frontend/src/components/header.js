af5f1c9fd9d8705e716eb2acea4a8d823b58bc70
crazynavi <117359800+TrustedSeven@users.noreply.github.com>
2023-04-11 18:01:22 -0500

    sign up, sign in button create

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,24 +1,37 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Button from '@mui/material/Button';
 
+import './header.css'
 
 function Header() {
-    return (
-      <>
-        <Navbar>
-          <Container>
-            <Nav>
-              <Nav.Link href="/">Casino</Nav.Link>
-              <Nav.Link href="/esports">eSports</Nav.Link>
-              <Nav.Link href="/lottery">Lottery</Nav.Link>
-              <Nav.Link href="/scratchcards">ScratchCards</Nav.Link>
-              <Nav.Link href="/virtual">Virtual</Nav.Link>
-            </Nav>
-          </Container>
-        </Navbar>
-      </>
-    );
-  }
-  
-  export default Header;
+  return (
+    <>
+      <div className="inline header-lay row">
+        <div className="col-md-2 mid">logo</div>
+        <div className="col-md-8 mid">
+          <Navbar>
+            <Container>
+              <Nav>
+                <Nav.Link href="/"><span className="mg-std">Casino</span></Nav.Link>
+                <Nav.Link href="/esports"><span className="mg-std">eSports</span></Nav.Link>
+                <Nav.Link href="/lottery"><span className="mg-std">Lottery</span></Nav.Link>
+                <Nav.Link href="/scratchcards"><span className="mg-std">ScratchCards</span></Nav.Link>
+                <Nav.Link href="/virtual"><span className="mg-std">Virtual</span></Nav.Link>
+              </Nav>
+            </Container>
+          </Navbar>
+        </div>
+
+        <div className="inline col-md-2 mid">
+          <Button variant="outlined" style={{marginRight:"1vw", color:"white"}}>Sign In</Button>
+          <Button variant="contained">Sign Up</Button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+
+export default Header;
